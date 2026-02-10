b05aa29b25c61e0399b9811ce3780feec7875c85
Siddak Singh Bhatia <siddak1234@gmail.com>
2026-02-09 22:46:45 -0600

    update icons

--- components/icons/processIcons.tsx
+++ components/icons/processIcons.tsx
@@ -72,7 +72,7 @@ export function MailSignalIcon(props: IconProps) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.6"
       className={triggerTileIconClasses}
       aria-hidden="true"
       {...props}
@@ -89,7 +89,7 @@ export function FileSignalIcon(props: IconProps) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.6"
       className={triggerTileIconClasses}
       aria-hidden="true"
       {...props}
@@ -108,7 +108,7 @@ export function StopwatchSignalIcon(props: IconProps) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      strokeWidth="2"
+      strokeWidth="1.6"
       className={triggerTileIconClasses}
       aria-hidden="true"
       {...props}
